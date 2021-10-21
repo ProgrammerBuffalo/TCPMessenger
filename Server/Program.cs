28f1cb9cc9f5c7718ad4260133440ff8d52540c3
ProgrammerBuffalo <eldareyvazov@gmail.com>
2021-10-21 16:12:11 +0400

    Project push

---
+++ Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+        static int serverPort = 3333;
+        static List<Socket> clients = new List<Socket>();
+
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(serverIP, serverPort); ;
+            serverSocket.Bind(serverEndPoint);
+            serverSocket.Listen(50);
+            Console.WriteLine("HOST STARTED");
+
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clients.Add(clientSocket);
+                Task.Run(() =>
+                {
+                    if (clientSocket.Connected)
+                    {
+                        Console.WriteLine($"{clientSocket.RemoteEndPoint} connected");
+
+                    }
+
+                    byte[] buffer = new byte[1024000];
+                    while (true)
+                    {
+                        try
+                        {
+                            for (int i = 0; i < clients.Count; i++)
+                            {
+                                int length = clientSocket.Receive(buffer);
+                                string message = Encoding.UTF8.GetString(buffer, 0, length);
+                                foreach (var client in clients)
+                                {
+                                    client.Send(Encoding.Unicode.GetBytes(message));
+                                }
+                            }
+
+                        }
+                        catch (SocketException)
+                        {
+                            Console.WriteLine($"{clientSocket.RemoteEndPoint} disconnected");
+                            break;
+                        }
+                    }
+                    clientSocket.Close();
+                });
+            }
+        }
+    }
+}
---
+++ SimpleMessenger/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleMessenger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IPAddress serverIp = IPAddress.Parse("127.0.0.1");
+        private int serverPort = 3333;
+        private Socket clientSocket;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(serverIp, serverPort));
+
+                if (clientSocket.Connected)
+                    btnConnect.IsEnabled = false;
+
+                Task.Run(() =>
+                {
+                    while (clientSocket.Connected)
+                    {
+                        byte[] buffer = new byte[1024000];
+                        int length = clientSocket.Receive(buffer);
+                        Dispatcher.Invoke(() => { txtBlock.Text += Encoding.UTF8.GetString(buffer, 0, length) + "\n"; });
+                    }
+                });
+
+            }
+            catch
+            {
+                clientSocket.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if ((clientSocket?.Connected).GetValueOrDefault())
+            {
+                clientSocket.Send(Encoding.UTF8.GetBytes(inputBox.Text));
+                inputBox.Clear();
+            }
+        }
+    }
+}
